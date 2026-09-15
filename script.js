d8320f5a2a2ef19dc6e23ae1b9274c47daec2bea
mariabonatomaia-cmd <maria.bonato.maia@escola.pr.gov.br>
2026-09-15 11:21:40 -0300

    atualizei o codigo

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ botoesCurtir.forEach(function(botaoCurtir){
             contador.textContent++;
             curtiu === true;
             else{
-                con.textContent--;
+                contador.textContent--;
                 curtiu =  false;
             }
         }
